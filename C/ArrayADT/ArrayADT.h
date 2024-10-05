e64e5ba8913f9e3878f3914b4e47da9bd36d977c
Saiyam-Sandhir-Jain <saiyam.sandhir.jain@gmail.com>
2024-10-05 21:57:34 +0530

    build C ArrayADT header file

---
+++ C/ArrayADT/ArrayADT.h
@@ -0,0 +1,36 @@
+#ifndef ARRAYADT_H
+#define ARRAYADT_H
+
+// ArrayADT Structure
+typedef struct ArrayADT {
+    int *_items;
+    unsigned int _size;
+    unsigned int _length;
+} Array;
+
+// Properties
+unsigned int getArraySize(Array *arrayADT); // Get '_size' attribute of Array
+unsigned int getArrayLength(Array *arrayADT); // Get '_length' attribute of Array
+
+// ArrayADT Constructors
+Array *createDefaultArray(); // Array Default Constructor
+Array *createArrayWithSize(unsigned int size); // Construct Array with 'size' parameter
+Array *copyArray(int arrayC[], unsigned int size); // Construct Array with 'arrayC' and 'size' parameters
+Array *copyArrayADT(Array *arrayADT); // Array Copy Constructor
+
+// Operations
+int getArrayElement(Array *arrayADT, unsigned int index); 
+void displayArray(Array *arrayADT);
+void appendArray(Array *arrayADT, int value);
+void extendArray(Array *arrayADT, Array *otherArray);
+void insertArray(Array *arrayADT, unsigned int index, int value);
+int popArray(Array *arrayADT);
+int removeArray(Array *arrayADT, unsigned int index);
+unsigned int removeValueFromArray(Array *arrayADT, int value);
+unsigned int linearSearchArray(Array *arrayADT, int value);
+void resizeArray(Array **arrayADT, unsigned int newSize);
+
+// ArrayADT Destructor
+void freeArray(Array **arrayADT);
+
+#endif // ARRAYADT_H
