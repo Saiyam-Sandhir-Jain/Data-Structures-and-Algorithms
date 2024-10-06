ba98308f2d96e91a4d52dc882b3bb5ee3009a7ce
Saiyam-Sandhir-Jain <saiyam.sandhir.jain@gmail.com>
2024-10-06 07:21:37 +0530

    Change the error handling flow

--- C/ArrayADT/ArrayADT.c
+++ C/ArrayADT/ArrayADT.c
@@ -6,86 +6,80 @@
 
 #define ERROR_VALUE INT_MAX  // Constant for error values
 
-enum MessageType {
-    NULL_ARRAY_ARGUMENT_ERROR,  // Error for null array argument
-    ARRAY_CREATION_ERROR,       // Error during array creation
-    NO_MESSAGE                  // No message
-};
-
-// Function to check if a pointer is null and print an error message if needed
-bool isNull(void *ptr, enum MessageType message) {
-    if (ptr) 
-        return false;
-
-    switch (message) {
-        case NULL_ARRAY_ARGUMENT_ERROR:
-            printf("Error: Invalid operation. Cannot operate on NULL Array.\n");
-            break;
-
-        case ARRAY_CREATION_ERROR:
-            printf("Error: Error occurred during array creation.\n");
-            break;
-
-        case NO_MESSAGE:
-        default:
-            break;
-    }
-
-    return true; 
-}
-
 // Get the size of the array
 unsigned int getArraySize(Array *arrayADT) {
-    if (isNull(arrayADT, NULL_ARRAY_ARGUMENT_ERROR)) 
-        return ERROR_VALUE;
-
-    return arrayADT->_size;
+    if (arrayADT)
+        return arrayADT->_size;
+    
+    // NULL pointer is passed
+    printf("Error: Cannot get size from NULL ArrayADT.\n");
+    return ERROR_VALUE;
 }
 
 // Get the length of the array
 unsigned int getArrayLength(Array *arrayADT) {
-    if (isNull(arrayADT, NULL_ARRAY_ARGUMENT_ERROR)) 
-        return ERROR_VALUE;
+    if (arrayADT)
+        return arrayADT->_length;
 
-    return arrayADT->_length;
+    // NULL pointer is passed
+    printf("Error: Cannot get length from NULL ArrayADT.\n");
+    return ERROR_VALUE;
+}
+
+// Get an element from the array
+int getArrayElement(Array *arrayADT, unsigned int index) {
+    if (arrayADT && index < arrayADT->_length)
+        return arrayADT->_items[index];
+
+    if (!arrayADT) // NULL pointer is passed
+        printf("Error: Cannot get element from NULL ArrayADT.\n");
+    else if (index >= arrayADT->_length) // Index is out of array bounds
+        printf("Error: Index out of ArrayADT bounds.\n");
+
+    return ERROR_VALUE;
 }
 
 // Constructor to create a default (empty) array
 Array *createDefaultArray() {
     Array *arrayADT = (Array *)malloc(sizeof(Array));
 
-    if (isNull(arrayADT, ARRAY_CREATION_ERROR))
-        return NULL;
+    if (arrayADT) {
+        arrayADT->_items = NULL; // Initialize items to NULL
+        arrayADT->_size = 0;     // Set size to 0
+        arrayADT->_length = 0;   // Set length to 0
 
-    arrayADT->_items = NULL; // Initialize items to NULL
-    arrayADT->_size = 0;     // Set size to 0
-    arrayADT->_length = 0;   // Set length to 0
+        return arrayADT;
+    }
 
-    return arrayADT; 
+    // Error during array creation
+    printf("Error: Error occured during ArrayADT creation.\n");
+    return NULL;
 }
 
 // Constructor to create an array with a specified size
 Array *createArrayWithSize(unsigned int size) {
     Array *arrayADT = (Array *)malloc(sizeof(Array));
-
-    if (isNull(arrayADT, ARRAY_CREATION_ERROR))
+    if (!arrayADT) { // Error during array creation
+        printf("Error: Error occured during ArrayADT creation.\n");
         return NULL;
-
-    // If size is zero, initialize the array as empty
-    if (size == 0) {
-        arrayADT->_items = NULL;
-        arrayADT->_size = 0;
-        arrayADT->_length = 0;
-        return arrayADT;
     }
 
-    // Allocate memory for the items in the array
-    arrayADT->_items = (int *)malloc(size * sizeof(int));
-    if (isNull(arrayADT->_items, ARRAY_CREATION_ERROR))
-        return NULL;
+    if (size == 0) {
+        arrayADT->_items = NULL;  // Initialize items to NULL
+        arrayADT->_size = 0;     // Set size to 0
+        arrayADT->_length = 0;   // Set length to 0
 
-    arrayADT->_size = size;   // Set size
-    arrayADT->_length = 0;    // Initial length is 0
+        return arrayADT;
+    }
+    
+    arrayADT->_items = (int *)malloc(size * sizeof(int));
+    if (!arrayADT->_items) { // Error during array creation
+        printf("Error: Error occured during array creation.\n");
+        return NULL;
+    }
+
+    arrayADT->_size = size; // Initialize size to 'size' parameter
+    arrayADT->_length = 0;  // Initialize length to 0
 
     return arrayADT; 
 }
